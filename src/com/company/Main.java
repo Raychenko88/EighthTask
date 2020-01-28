b49c1f4f6bd43c62ab5391aa4534abec6b1f94a6
Серега <sergeyraychenko@gmail.com>
2020-01-28 20:21:43 +0200

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,14 +8,6 @@ import com.company.service.WriteToFile;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        for (ConnectionToServer a : WriteToFile.writeInfo()){
-            System.out.println(a + " AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        }
-        for (String b : ReadFromFile.readInfo()){
-            System.out.println(b + " BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
-        }
-        for (ConnectionToServer c : CheckFilesByDate.deleteOldInfo(1)){
-            System.out.println(c + " CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
-        }
+        WriteToFile.writeInfo();
     }
 }
